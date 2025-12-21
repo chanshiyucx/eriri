33dc2e7befe75cf1acb261c237ebcf97a6e07d0c
chanshiyucx <chanshiyucx@gmail.com>
2025-12-21 18:14:29 +0100

    feat: color theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import './App.css'
+import '@/styles/tailwindcss.css'
 
 function App() {
   return (
     <main className="container">
       <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank" rel="noreferrer">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-      </div>
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
     </main>
   )
